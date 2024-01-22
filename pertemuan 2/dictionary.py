a8e0bb6e7fc6b25c2506626604d19d9da1584dbe
Aditya02pro <140993821+Aditya02pro@users.noreply.github.com>
2024-01-22 09:53:03 +0700

    Add files via upload

---
+++ pertemuan 2/dictionary.py
@@ -0,0 +1,8 @@
+dict = {"nama": "aditya", "kelas": "TKJ X A"}
+dict["nama"] = "aditya rama saputra"
+
+print(dict)
+print(dict["nama"])
+
+dict["sekolah"] = "SMK Medikacom"
+print(dict)
---
+++ pertemuan 2/latihan.py
@@ -0,0 +1,10 @@
+olahraga = ["voly","senam","badminton","lompat","basket","futsal"]
+
+olahraga_list = list(olahraga)
+olahraga_list.append ("tenis")
+print("update list =",olahraga_list)
+del olahraga_list[3]
+print("hapus list = ",olahraga_list)
+olahraga_list[4] = "sepakbola"
+print("update =", olahraga_list)
+mapel_tuple = tuple(olahraga_list)
