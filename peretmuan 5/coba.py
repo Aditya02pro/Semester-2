188a095c03d8558b324297f83dfeef268021aaac
Aditya02pro <140993821+Aditya02pro@users.noreply.github.com>
2024-02-12 09:49:01 +0700

    Add files via upload

---
+++ peretmuan 5/coba.py
@@ -0,0 +1,8 @@
+print("\mLOOPING IN PYTHON")
+print("--------------------\n")
+a = 0 
+b = float(input("masukan angka anda: "))
+while a < b: # a < b adalah syarat 
+    print(a)
+    a += 1 # Increment
+    
